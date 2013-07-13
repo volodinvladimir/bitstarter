a838d7f8f0e449f16da6b9a262b03730f768e089
Volodymyr Volodin <volodinvladimir@gmail.com>
2013-07-13 21:57:41 +0000

    web.js reads from index.html

--- web.js
+++ web.js
@@ -1,13 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   
-  var fs=require("fs");
-  var buffer = new Buffer =  fs.readFileSync('index.html');
-  var data = buffer.toString("utf8", 0 , buffer.length;
-  response.send(data);
+  response.send(fs.readFileSync(, 'utf8')); 
+ 
 });
 
 var port = process.env.PORT || 5000;
