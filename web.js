a876ee39aa2d5bdffcd9f9661e682db0167847c4
Volodymyr Volodin <volodinvladimir@gmail.com>
2013-07-13 21:43:31 +0000

    web.js reads from index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-
+  
+  var fs=require("fs");
   var data = fs.readFileSync('index.html')
   response.send(data);
 });
